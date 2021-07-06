cbe25837605a773758abecbe342af5543be797c1
ridvancanayakin <42276758+ridvancanayakin@users.noreply.github.com>
2021-07-06 21:15:30 +0300

    auth_service.dart dosyasında giriş yöntemleri toplandı

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -1,8 +1,41 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final _auth = FirebaseAuth.instance;
-  Future<UserCredential> signInwithCredential(AuthCredential credential) =>
-      _auth.signInWithCredential(credential);
+
+  Future<UserCredential> signInWithEmailAndPassword(email, password) async {
+    return await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
   Future<void> logout() => _auth.signOut();
+
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    return await _auth.signInWithCredential(credential);
+  }
+
+  Future<UserCredential> signInWithFacebook() async {
+    final LoginResult result = await FacebookAuth.instance.login();
+    // by default we request the email and the public profile
+// or FacebookAuth.i.login()
+    if (result.status == LoginStatus.success) {
+      // you are logged
+      final AccessToken accessToken = result.accessToken;
+      final facebookAuthCredential =
+          FacebookAuthProvider.credential(accessToken.token);
+      return await _auth.signInWithCredential(facebookAuthCredential);
+    }
+  }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,59 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:publist/auth_service.dart';
 import 'package:publist/components/rounded_button.dart';
+import 'package:publist/models/user_group_data.dart';
 import 'package:publist/screens/registration_screen.dart';
 import 'package:publist/constants.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:publist/screens/main_screen.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:auth_buttons/auth_buttons.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
-
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
   bool showSpinner = false;
-  final _auth = FirebaseAuth.instance;
+  final _auth = AuthService();
   String email;
   String password;
-
-  Future<UserCredential> signInWithGoogle() async {
-    // Trigger the authentication flow
-    final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
-
-    // Obtain the auth details from the request
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
-
-    // Create a new credential
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-
-    // Once signed in, return the UserCredential
-    return await _auth.signInWithCredential(credential);
-  }
-
-  Future<UserCredential> signInWithFacebook() async {
-    final LoginResult result = await FacebookAuth.instance.login();
-    // by default we request the email and the public profile
-// or FacebookAuth.i.login()
-    if (result.status == LoginStatus.success) {
-      // you are logged
-      final AccessToken accessToken = result.accessToken;
-      final facebookAuthCredential =
-          FacebookAuthProvider.credential(accessToken.token);
-      return await _auth.signInWithCredential(facebookAuthCredential);
-    } else {
-      return null;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -106,11 +72,9 @@ class _LoginScreenState extends State<LoginScreen> {
                   setState(() {
                     showSpinner = true;
                   });
-                  email = 'salih@email.com';
-                  password = '123456';
                   try {
-                    final user = await _auth.signInWithEmailAndPassword(
-                        email: email, password: password);
+                    final user =
+                        await _auth.signInWithEmailAndPassword(email, password);
                     if (user != null) {
                       Navigator.pushNamed(context, MainScreen.id);
                     }
@@ -131,9 +95,12 @@ class _LoginScreenState extends State<LoginScreen> {
                 //color: Color(0xFFD08933),
                 onPressed: () async {
                   try {
-                    final user = await signInWithGoogle();
+                    final user = await _auth.signInWithGoogle();
                     if (user != null) {
                       Navigator.pushNamed(context, MainScreen.id);
+                      print(user.user.displayName);
+                      print(user.user.email);
+                      print(user.user.uid);
                     }
                     setState(() {
                       showSpinner = false;
@@ -151,7 +118,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 //color: Color(0xFFD08933),
                 onPressed: () async {
                   try {
-                    final user = await signInWithFacebook();
+                    final user = await _auth.signInWithFacebook();
                     if (user != null) {
                       Navigator.pushNamed(context, MainScreen.id);
                       print(user.user.displayName);
@@ -168,7 +135,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 height: 25,
               ),
               RoundedButton(
-                title: "Dont have an account? Sign Up",
+                title: "Don't have an account? Sign Up",
                 color: Colors.grey,
                 onPressed: () {
                   Navigator.pushNamed(context, RegistrationScreen.id);
